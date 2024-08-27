c37bf9e748817d046355045f8790b9cd6956a761
Nathan Devey-Skeete <njds1676@gmail.com>
2024-08-27 10:51:03 +0100

    Added README and updated code

---
+++ README.md
@@ -0,0 +1,21 @@
+# Activity Bot
+
+Activity Bot for the Inquisitor discord
+
+## Functions
+
+• GetGoogleSheet - Returns the spreadsheet (either leadership or public)
+
+• CheckLoa - Returns a list of names who are not in a LOA or ROA
+
+• on_ready - Calls check_activity and shutsdown the bot
+
+• shutdown - Manual shutdown of the bot, at the moment, only Ari can use this
+
+• check_activity - Main function of the bot, pings inactive people
+
+## Spreadsheets
+
+• Public spreadsheet - Needed to check LOAs
+
+• Leadership spreadsheet - Needed for Discord IDs and last seen date
--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GOOGLE_SHEET_CREDENTIALS = os.getenv("GOOGLE_SHEETS_CREDENTIALS")
 ALLOWED_USER_IDS = [163199994919256064]
 
-# TODO update variables and ask Painite if we can use the bot
+# TODO update variables
 INQ_LEADERSHIP_SHEET_NAME = "Inquisitor Order"
 INQ_LEADERSHIP_SHEET_ID = "1QMduATDD2o0XG8P3gqMN7kVtPIpCGH_UEq3sgkwbAb8"
 INQ_PUBLIC_SHEET_ID = "15Ae2gh1rHMZx6WAm0tvCGqNHvYdtWjPl6rrQEpiDKB8"
@@ -35,7 +35,6 @@ def GetGoogleSheet(spreadsheet):
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         CredsInfo = json.loads(GOOGLE_SHEET_CREDENTIALS)
         creds = service_account.Credentials.from_service_account_info(CredsInfo, scopes=scope)
-       # creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_SHEET_CREDENTIALS, scope)
         client = gspread.authorize(creds)
         if spreadsheet == "leadership":
             sheet = client.open_by_key(INQ_LEADERSHIP_SHEET_ID).worksheet(INQ_LEADERSHIP_SHEET_NAME)
@@ -64,7 +63,7 @@ def CheckLoa():
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    await read_cell()
+    await check_activity()
     await bot.close()
 
 @bot.command(name="shutdown")
@@ -76,15 +75,15 @@ async def shutdown(ctx):
     else:
         await ctx.send("You don't have permission to shut down the bot")
 
-async def read_cell():
+async def check_activity():
     sheet = GetGoogleSheet("leadership")
     NameColumn = sheet.col_values(7)
     DiscordIDColumn = sheet.col_values(11) # TODO change to use letters
     DaysSinceColumn = sheet.col_values(12)
 
     NonLoaNames = CheckLoa()
-
     output = []
+
     for name, DiscordId, DaysSince in zip(NameColumn, DiscordIDColumn, DaysSinceColumn):
         if name != "" and name != "Name" and name != "dont delete":
             if name not in NonLoaNames:
